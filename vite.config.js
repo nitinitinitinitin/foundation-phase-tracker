751be0c0624a1f2e9c1f91f7b16c7b7f6fc06f5c
rayduskstudio <rayduskstudio@gmail.com>
2026-04-27 14:58:13 +0530

    adding cofigurations

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'foundation-phase-tracker' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/foundation-phase-tracker/',
+  base: '/',
 })
